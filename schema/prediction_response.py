6f2f7c59706608f074bd217f3477f27794320c83
Aryan Gupta <guptafamily3005@gmail.com>
2025-12-12 17:36:49 +0530

    first commit

---
+++ schema/prediction_response.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import Dict
+
+# ==================================== Data Validation for Response Model ==================================
+
+class PredictionResponse(BaseModel):
+    
+    predicted_categoty: str = Field(...,description="The predicted insurance premium category", example="High")
+    confidence: float = Field(...,description="Model's confidence score for the predicted class (range: 0 to 1)", example=0.8432)
+    class_probabilities: Dict[str, float] = Field(...,description="Probability distribution across all possible classes", example={"Low": 0.01, "Medium": 0.15, "High": 0.84}
+    )
